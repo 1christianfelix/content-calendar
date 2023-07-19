317d8f911c43d1f25ec8114269bdb979999814a7
Christian Felix <1christianfelix@gmail.com>
2023-07-18 18:26:45 -0700

    Added endpoint for listing all content

--- src/main/java/dev/christianfelix/contentcalendar/controller/ContentController.java
+++ src/main/java/dev/christianfelix/contentcalendar/controller/ContentController.java
@@ -3,10 +3,18 @@ package dev.christianfelix.contentcalendar.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.christianfelix.contentcalendar.repository.ContentCollectionRepository;
+
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
 
+	private final ContentCollectionRepository repository;
+
+	public ContentController(ContentCollectionRepository contentCollectionRepository) {
+		this.repository = contentCollectionRepository;
+	}
+	
 	
 	
 }
