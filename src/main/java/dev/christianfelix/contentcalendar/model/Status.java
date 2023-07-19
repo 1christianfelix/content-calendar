677055dd80e79863662e7a2e2b2e9a8ce2d82f31
Christian Felix <1christianfelix@gmail.com>
2023-07-18 17:33:02 -0700

    Createrd Content record and Status, Type enums

---
+++ src/main/java/dev/christianfelix/contentcalendar/model/Status.java
@@ -0,0 +1,8 @@
+package dev.christianfelix.contentcalendar.model;
+
+public enum Status {
+	IDEA,
+	IN_PROGRESS,
+	COMPLETED,
+	PUBLISHED,
+}
---
+++ src/main/java/dev/christianfelix/contentcalendar/model/Type.java
@@ -0,0 +1,8 @@
+package dev.christianfelix.contentcalendar.model;
+
+public enum Type {
+	ARTICLE,
+	VIDEO,
+	COURSE,
+	CONFERENCE_TALK
+}
