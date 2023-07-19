2dd0ecbb9dac500b217ea46b63379e18308ff0dd
Christian Felix <1christianfelix@gmail.com>
2023-07-18 18:45:11 -0700

    PostConstruct creation for initial content data

--- src/main/java/dev/christianfelix/contentcalendar/controller/ContentController.java
+++ src/main/java/dev/christianfelix/contentcalendar/controller/ContentController.java
@@ -1,8 +1,12 @@
 package dev.christianfelix.contentcalendar.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.christianfelix.contentcalendar.model.Content;
 import dev.christianfelix.contentcalendar.repository.ContentCollectionRepository;
 
 @RestController
@@ -15,6 +19,10 @@ public class ContentController {
 		this.repository = contentCollectionRepository;
 	}
 	
-	
+	// make a request and find all the pieces of content in the system
+	@GetMapping("")
+	public List<Content> findAll() {
+		return repository.findAll();
+	}
 	
 }
